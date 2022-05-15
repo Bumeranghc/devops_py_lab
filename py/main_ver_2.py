b03088e87a02a740b1b5c9fec50c698ed2bef262
Pavel Semchenko <pavelsemch@hotmail.com>
2022-05-15 21:05:27 +0300

    Patch and comment for version 2

--- py/main_ver_2.py
+++ py/main_ver_2.py
@@ -8,46 +8,36 @@ tmpl_slides_file="tmpl/slides_ver_2.md"
 out_md_file="out/slides_ver_2.md"
 out_pptx_file="out/slides_ver_2.pptx"
 
+# получить словари с описанием продуктов 
 products = ds.get_products()
-#print(*products)
+
+# использовать файл-шаблон
 with open(tmpl_slides_file, encoding="UTF-8") as file:
     product_template = file.read()
 
+# объединить файлы из этого списка в один в директории out
 filenames = []
-
 for product in products:
+    # подготовить шаблон для продукта к изменениям
     current_product_template = product_template
+    # сгенерировать имя для файла шаблона продукта
     current_template_file = "out/version_2." + str(product['product_id']) + ".md"
+    # добавить имя файла в список для объединения
     filenames.append(current_template_file)
+    # произвести замену переменных в шаблоне
     for key, value in product.items():
         current_product_template = current_product_template.replace('{'+key+'}', str(value))
+        # записать отредактированный шаблон в файл
         with open(current_template_file, "w") as outfile:
             outfile.write(current_product_template)
 
+# объединить файлы
 with open(out_md_file, "w", encoding="UTF-8") as outfile:
     for fname in filenames:
         with open(fname) as infile:
             for line in infile:
                 outfile.write(infile.read())
         outfile.write("\n")
-    #for key in product.keys():
-    #    print(key)
-    #    print(product[key])
-    #    test = product_template.replace('{'+key+'}', str(product[key]))
-    #    print(test)
-
-#for product in products:
-#    print(product)
-#    product_template.format(**product)
-
-# объединить файлы в один в директории out 
-#filenames = [tmpl_category_file, tmpl_slides_file]
-#with open(out_md_file, "w") as outfile:
-#    for fname in filenames:
-#        with open(fname) as infile:
-#            for line in infile:
-#                outfile.write(infile.read())
-#        outfile.write("\n")
 
 # преобразовать в pptx 
 m2o.convert_to_pptx(out_md_file, out_pptx_file) 
